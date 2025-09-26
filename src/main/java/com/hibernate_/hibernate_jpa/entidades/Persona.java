3f080ed86c199035dbac5d14cc15e34eb3411442
RONY RIVAS <ronyrivasmarquez@gamil.com>
2025-09-26 19:51:37 -0400

    se agrego un cometario

--- src/main/java/com/hibernate_/hibernate_jpa/entidades/Persona.java
+++ src/main/java/com/hibernate_/hibernate_jpa/entidades/Persona.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+// nuevo comentario 
 @Entity
 @Table  ( name = "personas")
 public class Persona {
